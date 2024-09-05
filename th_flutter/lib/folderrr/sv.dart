f56c59e751607f0be5fb086155a065a5cc95d8f8
Anhnt <tobi19932012@gmail.com>
2024-09-05 20:39:08 +0700

    tinh da hinh

--- th_flutter/lib/folderrr/sv.dart
+++ th_flutter/lib/folderrr/sv.dart
@@ -1,11 +1,29 @@
-class sinhVien {
+class SinhVien {
+  int agePublic = 100;
   int _age = 8;
-
   int get getAge {
-    return _age;
-  }
+    if (_age > 18) {
+      return 20;
+    }
+
+    return _age + 55;
+  } // => hàm getter
 
   set setAge(int age) {
-    _age = age + 19;
+    // => hàm setter
+    // xử lý logic ở đây
+    _age = age;
+  }
+
+  void updateAge(int age) {
+    // xử lý logic ở đây
+    _age = age;
+  }
+
+  String updateAge1(int age) {
+    // xử lý logic ở đây
+    _age = age;
+
+    return "abc";
   }
 }
